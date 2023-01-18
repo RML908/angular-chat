183fe84218257e3da1e10415fa7cd6891b17e18e
RafaelMutable <82508565+RafaelMutable@users.noreply.github.com>
2023-01-19 00:42:55 +0400

    fixed place pages components to correct place.

--- chat-angular/src/app/app-routing.module.ts
+++ chat-angular/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {LoginComponent} from "./models/auth/login/login.component";
+import {LoginComponent} from "./pages/auth/login/login.component";
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   {
-    path: 'Login', loadChildren: () => import('./models/auth/auth.module')
+    path: 'Login', loadChildren: () => import('./pages/auth/auth.module')
       .then(m => m.AuthModule)
   }
 ];
